73c56d82986a9189fa26964dec5ead5ef564cd09
Narendra Maurya <amanlearntoenvent17@gmail.com>
2024-05-13 16:55:57 +0530

    url update

--- controllers/order.controller.js
+++ controllers/order.controller.js
@@ -8,7 +8,7 @@ const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
 // placing user order for frontend
 const placeOrder = async(req,res) => {
 
-    const frontend_url = "https://nm-food.netlify.app/";
+    const frontend_url = "https://nm-food.netlify.app";
 
     try {
         const newOrder = new orderModel({
